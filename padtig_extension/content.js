ed909e2c34b89ad83e4028088358d47d3cc5cafa
툰라온 <hmt7136@gmail.com>
2020-05-11 11:42:33 +0900

    1.1 update

--- padtig_extension/content.js
+++ padtig_extension/content.js
@@ -1,5 +1,5 @@
 //변신 전
-var $normalskilldesc = $("div.tooltip-body dl.skillinfo dl.normal dl.description dd");
+var $normalskilldesc = $("#pad-info-monster-detail .skillinfo .normal .description dd");
 if ($normalskilldesc.length) {
 	var content = $normalskilldesc.text();
 	if(content.includes("변신")) {
@@ -11,13 +11,13 @@ if ($normalskilldesc.length) {
 		var name = content.substring(nameStartIndex + 1, nameEndIndex);
 		
 		$normalskilldesc.text(content.substring(0, nameStartIndex + 1));
-		$normalskilldesc.append("<a href='http://www.thisisgame.com/pad/info/monster/detail.php?code=" + num + "' class='yellowgreen'>" + name + "</a>");
+		$normalskilldesc.append("<a href='https://www.thisisgame.com/pad/info/monster/detail.php?code=" + num + "' class='yellowgreen'>" + name + "</a>");
 		$normalskilldesc.append(content.substring(nameEndIndex, content.length));
 	}
 }
 
 //변신 후
-var $extradesc = $("div.tooltip-body dl.extradescription dd");
+var $extradesc = $("#pad-info-monster-detail .extradescription dd");
 if ($extradesc.length) {
 	var content = $extradesc.text();
 	if($extradesc.text().includes("변신")) {
@@ -29,20 +29,21 @@ if ($extradesc.length) {
 		var name = content.substring(nameStartIndex + 1, nameEndIndex);
 		
 		$extradesc.text(content.substring(0, nameStartIndex + 1));
-		$extradesc.append("<a href='http://www.thisisgame.com/pad/info/monster/detail.php?code=" + num + "' class='yellowgreen'>" + name + "</a>");
+		$extradesc.append("<a href='https://www.thisisgame.com/pad/info/monster/detail.php?code=" + num + "' class='yellowgreen'>" + name + "</a>");
 		$extradesc.append(content.substring(nameEndIndex, content.length));
 	}
 }
 
 //프렌드 박스 등에서 일부 각성들이 OFF일 때 보이지 않는 현상 방지
-var $icons = $(".field-awake-icon img");
-if($icons.length) {
-	for(var i = 0; i < $icons.length; i++) {
-		var $icon = icons[i];
-		var iconSrc = $icon.attr("src");
-		if(iconSrc.contains("dark")) {
-			$icon.attr("src", iconSrc.split("dark")[0] + iconSrc.split("dark")[1]);
-			$icon.addClass("dark-icon");
-		}
-	}
-}
+// var $icons = $(".field-awake-icon img");
+// if($icons.length) {
+	// for(var i = 0; i < $icons.length; i++) {
+		// var $icon = icons[i];
+		// var iconSrc = $icon.attr("src");
+		// if(iconSrc.contains("dark")) {
+			// $icon.attr("src", iconSrc.split("dark")[0] + iconSrc.split("dark")[1]);
+			// $icon.addClass("dark-icon");
+		// }
+	// }
+// }
+
